100a660a4f9c2da5d648bc9c0f71b8a8b09cf6de
Mustafa Güler <mustfguler@hotmail.com>
2023-04-03 10:15:58 +0300

    Liste Üstüne Ekleme Yapıldı.

--- src/components/Header/TodoForm/index.js
+++ src/components/Header/TodoForm/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
 import validations from "./validitions";
+import { useTodo } from "../../../contexts/TodoContext";
 
 function TodoForm() {
+  const { addTodo } = useTodo();
+
   return (
     <Formik
       initialValues={{
@@ -10,6 +13,9 @@ function TodoForm() {
       }}
       onSubmit={async (values, bag) => {
         console.log(values);
+
+        addTodo(values.text);
+        
         bag.resetForm();
       }}
       validations={validations}
